a6e64af1b0c2b80ba2a198038ccae2be948dc54a
GCodez-503 <145606317+GCodez-503@users.noreply.github.com>
2026-06-20 11:29:06 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE = 'copado-bdr-v1';
+const ASSETS = [
+  '/Copado-BDR/',
+  '/Copado-BDR/index.html',
+  '/Copado-BDR/manifest.json',
+  '/Copado-BDR/icon-192.png',
+  '/Copado-BDR/icon-512.png',
+  '/Copado-BDR/icon-180.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }))
+  );
+});
